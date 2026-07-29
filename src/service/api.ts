33691449cc34e7a85d8252e288f3afec16d7363c
Anderson Mendes <andersonmendesdesouza2007@gmail.com>
2026-07-29 13:47:46 -0300

    merge:(merge) merge

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
-  // baseURL: "https://gilseppe-vidal-api.onrender.com",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://gilseppe-vidal-api.onrender.com",
 });
 
 api.interceptors.request.use(config => {
